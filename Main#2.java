1f22ec0633775d4f03c626e750df6c0e6a46ea94
popliarman2019 <42761596+popliarman2019@users.noreply.github.com>
2018-10-07 15:05:26 -0700

    Update Main#2.java

--- Main#2.java
+++ Main#2.java
@@ -18,70 +18,70 @@ public class Main {
 
         Random rand = new Random();
         double multiplier = rand.nextDouble() + 0.5;
-        double hit = multiplier * intelligence * 11;
+        double hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("You are facing a level 10 Orc with 100 Hit Points. \nType 'Play' to play turn 1.");
         sc.next();
         double hp1 = 1000-hit;
         System.out.println("The Orc has " + hp1 + "hit points left");
 
         multiplier = rand.nextDouble() + 0.5;
-        hit = multiplier * intelligence * 11;
+        hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("Type 'play' to play turn 2.");
         sc.next();
         double hp2 = hp1-hit;
         System.out.println("The Orc has " + hp2 + "hit points left");
 
         multiplier = rand.nextDouble() + 0.5;
-        hit = multiplier * intelligence * 11;
+        hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("Type 'play' to play turn 3.");
         sc.next();
         double hp3 = hp2 - hit;
         System.out.println("The Orc has " + hp3 + "hit points left");
 
         multiplier = rand.nextDouble() + 0.5;
-        hit = multiplier * intelligence * 11;
+        hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("Type 'play' to play turn 4.");
         sc.next();
         double hp4 = hp3-hit;
         System.out.println("The Orc has " + hp4 + "hit points left");
 
         multiplier = rand.nextDouble() + 0.5;
-        hit = multiplier * intelligence * 11;
+        hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("Type 'play' to play turn 5.");
         sc.next();
         double hp5 = hp4-hit;
         System.out.println("The Orc has " + hp5 + "hit points left");
 
         multiplier = rand.nextDouble() + 0.5;
-        hit = multiplier * intelligence * 11;
+        hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("Type 'play' to play turn 6.");
         sc.next();
         double hp6 = hp5-hit;
         System.out.println("The Orc has " + hp6 + "hit points left");
 
         multiplier = rand.nextDouble() + 0.5;
-        hit = multiplier * intelligence * 11;
+        hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("Type 'play' to play turn 7.");
         sc.next();
         double hp7 = hp6-hit;
         System.out.println("The Orc has " + hp7 + "hit points left");
 
         multiplier = rand.nextDouble() + 0.5;
-        hit = multiplier * intelligence * 11;
+        hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("Type 'play' to play turn 8.");
         sc.next();
         double hp8 = hp7-hit;
         System.out.println("The Orc has " + hp8 + "hit points left");
 
         multiplier = rand.nextDouble() + 0.5;
-        hit = multiplier * intelligence * 11;
+        hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("Type 'play' to play turn 9.");
         sc.next();
         double hp9 = hp8-hit;
         System.out.println("The Orc has " + hp9 + "hit points left");
 
         multiplier = rand.nextDouble() + 0.5;
-        hit = multiplier * intelligence * 11;
+        hit = multiplier * intelligence * 11; // the multiplier is multiplied times 11 and is therefore randomized between 5.5 to 16.5
         System.out.print("Type 'play' to play turn 10.");
         sc.next();
         double hp10 = hp9-hit;
